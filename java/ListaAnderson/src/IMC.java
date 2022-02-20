9249bacad899599fd718854afa179559d3e415c9
Murilo <murilopedroso@edu.unifil.br>
2022-02-19 21:38:59 -0300

    Identacao IMC

--- java/ListaAnderson/src/IMC.java
+++ java/ListaAnderson/src/IMC.java
@@ -1,19 +1,23 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class IMC {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
 
-        System.out.println("Digite o peso: ");
+        System.out.println("Digite o peso (ex: 80): ");
         double peso = teclado.nextDouble();
+        System.out.println("%n");
         
-        System.out.println("Digite a altura: ");
+        System.out.println("Digite a altura (ex: 1.80): ");
         double altura = teclado.nextDouble();
+        System.out.println("%n");
         
         double imc;
         imc = peso / Math.pow(altura, 2);
 
-        System.out.println("IMC e: " + imc);
+        System.out.println("IMC e: " + numberFormat.format(imc));
 
         if(imc < 18.5)
             System.out.println("Abaixo do peso");
@@ -28,6 +32,7 @@ public class IMC {
         else
             System.out.println("Obeso morbido");
 
+        teclado.close();
     }
 }
 
