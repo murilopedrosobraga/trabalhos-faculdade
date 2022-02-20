4040ce20198ea95fc7e3b5246d4cb7b204237fef
Murilo <murilopedroso@edu.unifil.br>
2022-02-19 22:36:56 -0300

    Antecessor

---
+++ java/ListaAnderson/src/antecessor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class antecessor {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Insira um valor: ");
+        int val = teclado.nextInt();
+
+        System.out.println("O valor " + val + ", tem como seu antecessor o numero: " + (val-1));
+
+    }
+}
